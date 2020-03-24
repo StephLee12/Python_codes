fce63f973cc74092a839c49b90eea9b9f07de5d8
StephLee12 <47514655+StephLee12@users.noreply.github.com>
2020-03-24 09:09:10 +0800

    Initial commit

---
+++ DM_Experiment1/feature_selection.py
@@ -0,0 +1,46 @@
+import numpy as np 
+from scipy.io import arff
+import pandas as pd
+
+# 加载数据
+def load():
+    dataset = arff.loadarff('DM_Experiment1/iris.arff')
+    df = pd.DataFrame(dataset[0])
+    columns = list(df)
+    last_column = columns.pop()
+
+    return df,columns,last_column
+#计算信息熵
+def entropy(df,att_name):
+    
+    new_data_att = df[att_name].unique()#去除重复值
+    ent = 0
+    for i in new_data_att:
+        pr = sum(df[att_name] == i) / df.shape[0] #计算概率
+        ent += pr * np.log(pr) #计算熵
+    
+    return -ent
+#计算条件信息熵
+def case_entropy(df,x_att,y_att):
+
+    new_x_att = df[x_att].unique()
+
+    pr_x = df[x_att].value_counts() / df.shape[0]
+    c_ent = 0
+
+    for i in new_x_att:
+        c_ent += pr_x[i] * entropy(df[df[x_att] == i], y_att)
+    return c_ent
+#计算信息增益  
+def IG(df,columns,last_column):
+    ig = []
+    ent = entropy(df,last_column)
+    for column in columns:
+        c_ent = case_entropy(df,column,last_column)
+        ig.append(ent - c_ent)
+    return ig
+    
+if __name__ == "__main__":
+    df,columns,last_column= load()
+    ig = IG(df,columns,last_column)
+    print(ig)
