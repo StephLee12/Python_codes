3f37f1c5a39c9b3993068d374cf2833c31a80b45
StephLee12 <47514655+StephLee12@users.noreply.github.com>
2020-05-04 12:09:56 +0800

    Initial Commit

--- Data_Structure_Algorithms/图/graph.py
+++ Data_Structure_Algorithms/图/graph.py
@@ -47,7 +47,7 @@ class Graph:
         edges = []
         for i in range(len(row)):
             if row[i] != unconn:
-                edges.append((i,row[i]))
+                edges.append((i,row[i])) # append是一个元组 第一个elem是顶点的index 第二个elem是权重
         
         return edges
         
@@ -77,10 +77,10 @@ class GraphAL:
         elif self.index_invalid(vi) or self.index_invalid(vj):
             raise GraphError(str(vi) + 'or' + str(vj) + "is not a valid index")
         for i in range(len(self._list[vi])):
-            if self._list[vi][i][0] == vj:
+            if self._list[vi][i][0] == vj: # self._list[vi][i]是一个元组
                 print("modify the value of edge"+ str(vi)+' '+str(vj))
                 self._list[vi][i][1] = val
-            elif self._list[vi][i][0] > vj:
+            elif self._list[vi][i][0] > vj: # 如果某一个顶点的index比vj还大 说明vi和vj之间没有边
                 break
         print("Create a new edge between vertices" + str(vi) + "and" + str(vj))
         self._list[vi].insert(i,(vj,val))
@@ -102,7 +102,7 @@ class GraphAL:
             raise GraphError(str(vi) + "is not a valid index")
         return self._list[vi]
 
-def DFS_graph(graph,v0):
+def DFS_graph_non_recursive(graph,v0): # 借用辅助栈 非递归实现
     ver_num = graph.get_ver_num()
     visited = [0] * ver_num
     visited[v0] = 1
@@ -118,6 +118,18 @@ def DFS_graph(graph,v0):
                 dfs_seq.append(v)
                 visited[v] = 1
                 st.push((0,graph.get_outedges(v)))
+    return dfs_seq
+
+def DFS_graph_recursive(graph,v,visited,dfs_seq): 
+   # 最初输入的visited是一个全0的列表 dfs_seq初始输入为[]
+    dfs_seq.append(v) #将v添加的dfs序列
+    visited[v] = 1 # 标记v顶点
+    v_outedges = graph.get_outedges(v) # 获取出度 是一个元组
+    for edge in v_outedges:
+        v_next_index = edge[0] 
+        if visited[v_next_index] != 1: # 没有被访问过
+            DFS_graph_recursive(graph,v_next_index,visited,dfs_seq)
+    
     return dfs_seq
 
 def BFS_graph(graph,v0):
@@ -140,6 +152,62 @@ def BFS_graph(graph,v0):
                 bfs_seq.append(v)
     
     return bfs_seq
+
+def get_missing_vertex(graph,dfs_seq): #获取未访问的顶点列表
+    miss_list = []
+    full_list = [i for i in range(graph.get_ver_num())]
+    for elem in full_list:
+        if elem not in dfs_seq:
+            miss_list.append(elem)
+    
+    return miss_list
+
+def has_traversed(graph,dfs_seq): #判断图是否完全遍历
+
+    if len(dfs_seq) != graph.get_ver_num():
+        return False
+    else:
+        return True
+
+def dfs_continue_traverse(graph,miss_list): #若图未遍历完全 继续遍历
+
+    dfs_bucket = []
+    for i in range(len(miss_list)):
+        # 以新顶点遍历图 用递归或非递归都可
+        dfs_seq = DFS_graph_recursive(graph,miss_list[i],[0]*graph.get_ver_num(),[]) 
+        dfs_bucket.append(dfs_seq)
+        # 在原miss_list弹出该顶点
+        miss_list.pop(i)
+        # 获取新的miss_list
+        new_miss_list = get_missing_vertex(graph,dfs_seq)
+        # 若原miss_list中的元素在新miss_list有出现，要弹出
+        for j in range(len(miss_list)):
+            if miss_list[j] in new_miss_list:
+                miss_list.pop(j)
+        if miss_list == []:
+            break
+    return dfs_bucket
+
+# 有点懒 就把dfs的复制一下 改个变量名
+def bfs_continue_traverse(graph,miss_list): #若图未遍历完全 继续遍历
+
+    bfs_bucket = []
+    for i in range(len(miss_list)):
+        # 以新顶点遍历图
+        bfs_seq = BFS_graph(graph,miss_list[i])
+        bfs_bucket.append(bfs_seq)
+        # 在原miss_list弹出该顶点
+        miss_list.pop(i)
+        # 获取新的miss_list
+        new_miss_list = get_missing_vertex(graph,bfs_seq)
+        # 若原miss_list中的元素在新miss_list有出现，要弹出
+        for j in range(len(miss_list)):
+            if miss_list[j] in new_miss_list:
+                miss_list.pop(j)
+        if miss_list == []:
+            break
+    return bfs_bucket
+
 
 def DFS_SpanTree(graph,v0):
     vertex_num = graph.get_ver_num()
@@ -184,16 +252,16 @@ def BFS_SpanTree(graph,v0):
     
     return bfs_spantree
 
-def Kruskal_MST(graph):
+def Kruskal_MST(graph): #选择权重最小的连通分量相连
     vertex_num = graph.get_ver_num()
-    reps = [i for i in range(vertex_num)]
+    reps = [i for i in range(vertex_num)] #代表元
     mst,edges=[],[]
     
     for vi in range(vertex_num):
         for v , w in graph.get_outedges(vi):
-            edges.append((w,vi,v))
+            edges.append((w,vi,v)) 
 
-    edges.sort()
+    edges.sort() #边按权重排序
 
     for w, vi ,vj  in edges :
         if reps[vi] != reps[vj]:
@@ -201,10 +269,10 @@ def Kruskal_MST(graph):
             
             if len(mst) == vertex_num -1 :
                 break
-
+            
             for i in range(vertex_num):
                 if reps[i] == reps[vj]:
-                    reps[i] = reps[vj]
+                    reps[i] = reps[vi] #修改元 合并连通分量
     
     return mst
 
@@ -218,14 +286,15 @@ def Prim_MST(graph,v0):
     while count < vertex_num and not prio_qu.is_empty():
         w, prev_v ,v = prio_qu.dequeue()
 
-        if mst[v] is not None:
-            continue 
+        if mst[v] is not None: #判断顶点v是否在已连接的树中
+            continue
+        #若顶点v不在 已经生成的树中 则prev_v和v构成的边一定在最小生成树上 
         mst[v] = ((prev_v,v),w)
         count = count + 1
 
         for vertex, weight in graph.get_outedges(v):
             if mst[vertex] is None:
-                prio_qu.enqueue((weight,prev_v,vertex))
+                prio_qu.enqueue((weight,prev_v,vertex)) #优先队列入列后会排序
     
     return mst
 
@@ -257,6 +326,12 @@ def Floyd(graph):
 
     inf = float("inf")
 
+    #for i in range(vertex_num):
+    #   for j in range(vertex_num):
+    #       if a[i][j] == inf:
+    #           next_v[i][j] = -1
+    #       else:
+    #           next_v[i][j] = j
     next_v = [ [-1 if a[i][j]==inf else j for j in range(vertex_num)] for i in range(vertex_num)]
 
     for k in range(vertex_num):
